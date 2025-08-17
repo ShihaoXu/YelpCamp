1eb732b004f94a5787b8141e0d6b8536f92e26c1
Shihao Xu <konto.shxu@gmail.com>
2025-08-17 23:33:26 +0200

    Add routes for update and delete
    Also add dependency: `method-override`
    
    Signed-off-by: Shihao Xu <konto.shxu@gmail.com>

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground')
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     .catch(error => console.error("Initial connection to yelp-camp failed."));
@@ -18,6 +19,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 // Routes
 app.get('/', (req, res) => {
@@ -48,6 +50,22 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render('campgrounds/show', { campground });
 })
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit', { campground });
+})
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndUpdate(id, req.body.campground);
+    res.redirect(`/campgrounds/${id}`);
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
 app.listen(3000, () => {
     console.log("Serving on port 3000.");
 })
