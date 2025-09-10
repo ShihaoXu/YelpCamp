93a318854678c835e719b81be3f27fc3f8dff31e
Shihao Xu <konto.shxu@gmail.com>
2025-09-10 23:41:23 +0200

    Add multer middleware for file upload
    Signed-off-by: Shihao Xu <konto.shxu@gmail.com>

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,10 +3,15 @@ const express = require('express');
 const router = express.Router();
 const campgroundsController = require('../controllers/campgrounds');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware')
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' }); // a local folder will accomodate the uploaded files
 
 router.route('/')
     .get(catchAsync(campgroundsController.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgroundsController.create));
+    // .post(isLoggedIn, validateCampground, catchAsync(campgroundsController.create));
+    .post(upload.single('image'), (req, _res) => { // upload.array() can upload multiple files; check the docs
+        console.log(req.body, req.file);
+    });
 
 router.get('/new', isLoggedIn, campgroundsController.renderNewForm);
 
